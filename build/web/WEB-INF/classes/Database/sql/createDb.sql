c65f342bd45c6ffdb672cc1c3860aa0ed4f80c3e
toucheqt <xkrpecqt@gmail.com>
2014-12-03 15:47:32 +0100

    finished refactoring db

--- build/web/WEB-INF/classes/Database/sql/createDb.sql
+++ build/web/WEB-INF/classes/Database/sql/createDb.sql
@@ -53,15 +53,15 @@ CREATE TABLE examination (
     id INT AUTO_INCREMENT,
     description VARCHAR(150) NOT NULL,
     examTime DATE NOT NULL,
-    hospitalizationId INT NOT NULL,
-    doctor VARCHAR(35),
+    patientId INT NOT NULL,
+    doctor VARCHAR(35) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (hospitalizationId) REFERENCES hospitalization(id),
+    FOREIGN KEY (patientId) REFERENCES patients(id),
     FOREIGN KEY (doctor) REFERENCES usertable(email)
 );
 
 CREATE TABLE isWorking (
-    tel NUMERIC(9),
+    tel VARCHAR(15),
     workingTime VARCHAR(15) NOT NULL,
     departmentNum INT NOT NULL,
     doctor VARCHAR(35) NOT NULL,
@@ -85,7 +85,7 @@ CREATE TABLE nurse (
 CREATE TABLE prescriptions (
     id INT AUTO_INCREMENT,
     startTime DATE NOT NULL,
-    endTime DATE NOT NULL,
+    endTime DATE,
     dosage VARCHAR(30) NOT NULL,
     patientId INT NOT NULL,
     drugId INT NOT NULL,
@@ -127,17 +127,50 @@ INSERT INTO department (depName) VALUES ("Urologie");
 INSERT INTO department (depName) VALUES ("Chirurgie");
 INSERT INTO department (depName) VALUES ("Traumatologie");
 
+-- INSERT NURSES
+INSERT INTO nurse (username, surname, birthNum, address, city, departmentNum)
+VALUES ("Lenka", "Páčková", "926453/1354", "Na Louce 13, 611 00", "Šlapanice", 1);
+INSERT INTO nurse (username, surname, birthNum, address, city, departmentNum)
+VALUES ("Pavlína", "Orlová", "856645/5642", "Příčná 245, 612 00", "Brno", 2);
+INSERT INTO nurse (username, surname, birthNum, address, city, departmentNum)
+VALUES ("Stanislav", "Nechtěný", "936542/4521", "Božetěchova 5, 612 00", "Brno", 5);
+INSERT INTO nurse (username, surname, birthNum, city, departmentNum)
+VALUES ("Tereza", "Zelná", "756423/7254", "Kroměříž", 3);
+INSERT INTO nurse (username, surname, birthNum, address, city, departmentNum)
+VALUES ("Anna", "Bočková", "854621/5432", "ˇŘečkovice 5641, 612 00", "Brno", 4);
+INSERT INTO nurse (username, surname, birthNum, departmentNum)
+VALUES ("Matylda", "Zett", "845612/5454", 2);
+INSERT INTO nurse (username, surname, birthNum, address, city, departmentNum)
+VALUES ("Barbora", "Svatá", "846532/4568", "Hrnčířská 12/134, 612 00", "Brno", 1);
+
+-- INSERT ISWORKING
+INSERT INTO isworking (tel, workingTime, departmentNum, doctor)
+VALUES ("576 234 516", "Plný úvazek", 1, "zelda@example.com");
+INSERT INTO isworking (tel, workingTime, departmentNum, doctor)
+VALUES ("586 456 452", "Plný úvazek", 2, "kulda@naveleslavine.cz");  
+INSERT INTO isworking (workingTime, departmentNum, doctor)
+VALUES ("Internship", 2, "queen@arrow.com");  
+INSERT INTO isworking (tel, workingTime, departmentNum, doctor)
+VALUES ("534 264 345", "Částečný úvazek", 3, "kulda@naveleslavine.cz");
+INSERT INTO isworking (tel, workingTime, departmentNum, doctor)
+VALUES ("512 546 241", "Plný úvazek", 4, "zatop@seznam.cz");
+INSERT INTO isworking (workingTime, departmentNum, doctor)
+VALUES ("Plný úvazek", 5, "p.novak@seznam.cz");
+INSERT INTO isworking (workingTime, departmentNum, doctor)
+VALUES ("Internship", 5, "nemc@gmail.com");
+
+
 -- INSERT DRUGS
 INSERT INTO drugs (drugName, description, effectivity, contraindication) 
-VALUES ('Kinedryl', 'Lék při léčbě nevolností', 'moxastini tenoclas', 'Ospalost,slabost,podrážděnost');
+VALUES ('Kinedryl', 'Lék při léčbě nevolností', 'moxastini tenoclas', 'Ospalost, slabost, podrážděnost');
 INSERT INTO drugs (drugName, description, effectivity, contraindication)  
 VALUES('Aspirin', 'Lék při léčbě bolesti a chřipky', 'kyselina acetylsaciliová', 'Zvracení, dušnost');
 INSERT INTO drugs (drugName, description, effectivity, contraindication) 
-VALUES('Paralen', 'Lék při léčbě bolesti a chřipky', 'Paracetamol', 'Ospalost,slabost');
+VALUES('Paralen', 'Lék při léčbě bolesti a chřipky', 'Paracetamol', 'Ospalost, slabost');
 INSERT INTO drugs (drugName, description, effectivity, contraindication) 
-VALUES('Endiaron', 'Lék při léčbě průjmů', 'Cloroxinum', 'Ospalost,svalové stahy,podrážděnost');
+VALUES('Endiaron', 'Lék při léčbě průjmů', 'Cloroxinum', 'Ospalost, svalové stahy, podrážděnost');
 INSERT INTO drugs (drugName, description, effectivity, contraindication) 
-VALUES('Ibalgin', 'Lék při léčbě bolesti a zánětu', 'ibuprofen', 'slabost,podrážděnost');
+VALUES('Ibalgin', 'Lék při léčbě bolesti a zánětu', 'ibuprofen', 'Slabost, podrážděnost');
 
 -- INSERT PATIENTS
 INSERT INTO patients (patientName, surname, birthNum, address, city) 
@@ -157,7 +190,7 @@ VALUES ("Oliver", "Fiat", "559542/4584", "Moravské náměstí 592, 612 00", "Br
 INSERT INTO patients (patientName, surname, birthNum, address, city) 
 VALUES ("Stanislav", "Nechutný", "685435/4568", "Veleslavínova 592, 612 00", "Brno");
 
--- INSERT HOSPITALIYATION
+-- INSERT HOSPITALIZATION
 INSERT INTO hospitalization (hospitalized, released, patientId, departmentNum, doctor)
 VALUES ("2014-02-07", "2014-03-02", 2, 1, "zelda@example.com");
 INSERT INTO hospitalization (hospitalized, patientId, departmentNum, doctor)
@@ -171,3 +204,50 @@ VALUES ("2014-12-01", 2, 1, "zelda@example.com");
 INSERT INTO hospitalization (hospitalized, released, patientId, departmentNum, doctor)
 VALUES ("2014-05-21", "2014-05-27", 6, 5, "p.novak@seznam.cz");
 
+-- INSERT PRESCRIPTIONS
+INSERT INTO prescriptions (startTime, endTime, dosage, patientId, drugId)
+VALUES ("2014-05-21", "2014-06-01", "ráno", 6, 4);
+INSERT INTO prescriptions (startTime, endTime, dosage, patientId, drugId)
+VALUES ("2014-10-08", "2014-11-01", "ráno-večer", 2, 1);
+INSERT INTO prescriptions (startTime, endTime, dosage, patientId, drugId)
+VALUES ("2013-01-01", "2013-01-07", "poledne", 2, 5);
+INSERT INTO prescriptions (startTime, dosage, patientId, drugId)
+VALUES ("2014-05-12", "ráno-poledne-večer", 4, 3);
+INSERT INTO prescriptions (startTime, endTime, dosage, patientId, drugId)
+VALUES ("2014-03-27", "2014-04-10", "ráno-večer", 7, 2);
+INSERT INTO prescriptions (startTime, endTime, dosage, patientId, drugId)
+VALUES ("2014-05-11", "2014-06-29", "večer", 1, 3);
+INSERT INTO prescriptions (startTime, endTime, dosage, patientId, drugId)
+VALUES ("2014-07-23", "2014-08-23", "ráno-večer", 3, 4);
+
+-- INSERT EXAMINATION
+INSERT INTO examination (description, examTime, patientId, doctor)
+VALUES ("Infarkt", "2014-02-07", 2, "zelda@example.com"); 
+INSERT INTO examination (description, examTime, patientId, doctor)
+VALUES ("Problémy s erekcí", "2014-05-12", 6, "p.novak@seznam.cz");
+INSERT INTO examination (description, examTime, patientId, doctor)
+VALUES ("Bolest loket", "2014-05-11", 4, "zatop@seznam.cz");
+INSERT INTO examination (description, examTime, patientId, doctor)
+VALUES ("Sedřená kůže", "2014-07-23", 1, "p.novak@seznam.cz");
+INSERT INTO examination (description, examTime, patientId, doctor)
+VALUES ("Zlomená pravá paže", "2014-10-08", 3, "kulda@naveleslavine.cz");
+INSERT INTO examination (description, examTime, patientId, doctor)
+VALUES ("Výron kotníku", "2013-01-01", 5, "nemc@gmail.com");
+INSERT INTO examination (description, examTime, patientId, doctor)
+VALUES ("Infarkt", "2014-08-09", 2, "zelda@example.com");
+
+-- INSERT RESULTS
+INSERT INTO results (resultDate, resultDsc, examinationId)
+VALUES ("2014-02-10", "Prasklá aorta", 1);
+INSERT INTO results (resultDate, resultDsc, examinationId)
+VALUES ("2014-05-12", "Impotence", 2);
+INSERT INTO results (resultDate, resultDsc, examinationId)
+VALUES ("2014-05-14", "Tenisový loket", 3);
+INSERT INTO results (resultDate, resultDsc, examinationId)
+VALUES ("2014-07-26", "Tuberkulóza", 4);
+INSERT INTO results (resultDate, resultDsc, examinationId)
+VALUES ("2014-10-08", "Zlomenina kosti", 5);
+INSERT INTO results (resultDate, resultDsc, examinationId)
+VALUES ("2013-01-05", "Přetrhnutý vnitřní vaz kotníku", 6);
+INSERT INTO results (resultDate, resultDsc, examinationId)
+VALUES ("2014-08-12", "ZPrasklá aorta", 7);
--- build/web/WEB-INF/classes/Database/sql/isWorking.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE isWorking (
-    tel NUMERIC(9),
-    workingTime VARCHAR(15) NOT NULL,
-    departmentNum INT NOT NULL,
-    doctor VARCHAR(35) NOT NULL,
-    FOREIGN KEY (departmentNum) REFERENCES department(id),
-    FOREIGN KEY (doctor) REFERENCES usertable(email),
-    PRIMARY KEY (departmentNum, doctor)
-);
